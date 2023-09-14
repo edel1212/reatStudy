090ab894408b31225c301d06674aa903d73c78e6
yoo <edel1212@naver.com>
2023-09-14 22:23:20 +0900

    add App Module css

--- react-for-middle/my-app/src/App.js
+++ react-for-middle/my-app/src/App.js
@@ -1,10 +1,12 @@
 // 만든 버튼을 Import
 import Button from "./Button";
 
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h1>Yeeeee~</h1>
+      <h1 className={styles.title}>Yeeeee~</h1>
       <Button text="hi!sss22" />
     </div>
   );
