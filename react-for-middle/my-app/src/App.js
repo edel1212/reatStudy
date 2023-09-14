84ee74818b33eb91ca8f8dc2ce96f91baf29839b
yoo <edel1212@naver.com>
2023-09-14 21:47:04 +0900

    add yarn

--- react-for-middle/my-app/src/App.js
+++ react-for-middle/my-app/src/App.js
@@ -1,7 +1,11 @@
+// 만든 버튼을 Import
+import Button from "./Button";
+
 function App() {
   return (
     <div>
-      <h1>Hi</h1>
+      <h1>Yeeeee~</h1>
+      <Button text="hi!" />
     </div>
   );
 }
---
+++ react-for-middle/my-app/src/Button.js
@@ -0,0 +1,5 @@
+function Button({ text }) {
+  return <button>{text}</button>;
+}
+// Export 시켜줌
+export default Button;
