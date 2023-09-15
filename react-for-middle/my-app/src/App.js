c987779206cd4ce0bf6b32afa4256a3c9fe711d7
yoo <edel1212@naver.com>
2023-09-15 22:10:53 +0900

    일반 usesate()사용

--- react-for-middle/my-app/src/App.js
+++ react-for-middle/my-app/src/App.js
@@ -3,11 +3,20 @@ import Button from "./Button";
 
 import styles from "./App.module.css";
 
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+  const clickEvent = () => {
+    console.log("call api!!!");
+    setCount((current) => current + 1);
+  };
   return (
     <div>
+      {count}
       <h1 className={styles.title}>Yeeeee~</h1>
       <Button text="hi!sss22" />
+      <button onClick={clickEvent}>count</button>
     </div>
   );
 }
