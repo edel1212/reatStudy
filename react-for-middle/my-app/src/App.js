d526c51a45da64c0e853dbaa289b74c6b2dd5c58
yoo <edel1212@naver.com>
2023-09-19 22:27:27 +0900

    Router 적용

--- react-for-middle/my-app/src/App.js
+++ react-for-middle/my-app/src/App.js
@@ -1,9 +1,27 @@
+import {
+  BrowserRouter as Router,
+  //HashRouter as Router,  주소에 #이 붙는다
+  Routes,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/moive">
+            <Detail />
+          </Route>
+          {/* exact사용 이유는 home경로를 정확하게 이해시키기 위함 */}
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
