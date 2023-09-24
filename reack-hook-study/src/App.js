d88d62f8cf45d02292b9d44465335b04291c085a
yoo <edel1212@naver.com>
2023-09-24 17:53:25 +0900

    useSate 활용 - 외부에서 불러와 사용 하기

--- reack-hook-study/src/App.js
+++ reack-hook-study/src/App.js
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
+// 💬 외부에 선언 되어 있는 함수안에 useStat가 있음
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  return { value, setValue };
+};
+
 function App() {
-  return <div className="App">Hi</div>;
+  // 💬 컴포넌트 내부 함수에서 외부 함수를 선언해서 반환 Object를 받음
+  const name = useInput("yoo");
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="name"
+        // 👉 Object 형식으로 반환 하기 떄문에 아래와 같이 사용
+        value={name.value}
+        onChange={(e) => name.setValue(e.target.value)}
+      />
+    </div>
+  );
 }
 
 export default App;
